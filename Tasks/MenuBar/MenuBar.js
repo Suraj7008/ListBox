f7f63faf5aace4f4e823a3c477c200da934d3cee
Suraj7008 <99787982+Suraj7008@users.noreply.github.com>
2023-07-13 16:29:08 +0530

    updated

--- Tasks/MenuBar/MenuBar.js
+++ Tasks/MenuBar/MenuBar.js
@@ -1,175 +1,89 @@
-document.addEventListener('keydown', handleNavigation, handleSubmenuNavigation);
+document.addEventListener('keydown', handleKeyDown);
 
-    // Function to handle keyboard navigation
-    function handleNavigation(event) {
-    const currentMenuItem = event.target;
-    const parentMenuItem = currentMenuItem.parentElement;
-    const siblingMenuItems = Array.from(parentMenuItem.parentElement.children);
-    const currentIndex = siblingMenuItems.indexOf(parentMenuItem);
-
+function handleKeyDown(event) {
+    const menubar = document.getElementById('menubar');
+    const currentLink = event.target;
+    const parentItem = currentLink.parentNode.parentNode;
+    const focusedElement = document.activeElement;
+    const isSubMenuOpen = focusedElement.parentElement.classList.contains('submenu');
+    let nextElement;
+    const submenu = parentItem.querySelector('.submenu');
+  const submenuitem = parentItem.querySelector('.submenuitem')
     switch (event.key) {
-        case 'ArrowLeft':
-            event.preventDefault();
-            if (currentIndex > 0) {
-                siblingMenuItems[currentIndex - 1].querySelector('.menu').focus();
+      case 'ArrowRight':
+        if (!isSubMenuOpen) {
+            nextElement = focusedElement.parentElement.nextElementSibling;
+            if (nextElement) {
+              nextElement.querySelector('a').focus();
+              event.preventDefault();
             }
+          } else {
+            if (isSubMenuOpen) {
+                submenuitem.style.display='flex';
+                focusedElement.nextElementSibling.querySelector('a').focus();
+                event.preventDefault();
+              }
+          }
         break;
-
-        case 'ArrowRight':
+      case 'ArrowLeft':
+        if (!isSubMenuOpen) {
+          const previousElement = focusedElement.parentElement.previousElementSibling;
+          if (previousElement) {
+            previousElement.querySelector('a').focus();
             event.preventDefault();
-            if (currentIndex < siblingMenuItems.length - 1) {
-                siblingMenuItems[currentIndex + 1].querySelector('.menu').focus();
-                event.stopPropagation();
+          }
+        } else {
+            const prevItem = parentItem.previousElementSibling;
+            const prevLink = prevItem.querySelector('a');
+            const Esub = document.querySelectorAll('.menu');
+                if (prevItem) {
+                    prevLink.focus();
+                    Esub[1].setAttribute('aria-expanded', 'false');
+                    Esub[2].setAttribute('aria-expanded', 'false');
+                    // submenu.style.display = 'none';
+                    event.preventDefault();
+                }
+          }
+
+        break;
+      case 'ArrowDown':
+        if (isSubMenuOpen) {
+          nextElement = focusedElement.nextElementSibling;
+          if (nextElement) {
+            nextElement.focus();
+            event.preventDefault();
+          }
+        } else {
+          const submenu = focusedElement.nextElementSibling;
+          if (submenu && submenu.classList.contains('submenu')) {
+            submenu.classList.add('open');
+            submenu.querySelector('a').focus();
+            event.preventDefault();
+          }
+        }
+        break;
+      case 'ArrowUp':
+        if (!isSubMenuOpen) {
+            const previousElement = focusedElement.parentElement.previousElementSibling;
+            if (previousElement) {
+              previousElement.querySelector('a').focus();
+              event.preventDefault();
             }
-        break;
-
-        case 'ArrowDown':
-            const submenu = parentMenuItem.querySelector('.submenu');
-            const sub = document.querySelectorAll('.menu');
-                if (submenu) {
-                    event.preventDefault();
-                    submenu.style.display = 'block';
-                    const submenuLinks = submenu.querySelectorAll('a');
-                    if (submenuLinks.length > 0) {
-                        submenu.style.display = 'block';
-                        submenuLinks[0].focus();
-                        currentMenuItem.setAttribute('aria-expanded','true');
-                        sub[0].setAttribute('tabindex','-1');
-                    }
-                }
-        break;
-
-        case 'ArrowUp':
-            const Usubmenu = parentMenuItem.querySelector('.submenu');
-                if (Usubmenu && Usubmenu.style.display !== 'block') {
-                    event.preventDefault();
-                    Usubmenu.style.display = 'block';
-                    const submenuLinks = Usubmenu.querySelectorAll('a');
-                    if (submenuLinks.length > 0) {
-                        document.querySelector('.submenu1').focus();
-                        document.querySelector('.submenu2').focus();
-                        
-                    } 
-                }
+          } else {
+            const previousElement = focusedElement.previousElementSibling;
+            if (previousElement) {
+              previousElement.focus();
+              event.preventDefault();
+            }
+          }
         break;
 
         case 'Escape':
-            event.preventDefault();      
-            // closeSubmenus();
-                const parentItem = parentMenuItem.previousElementSibling;
-                const Esub = document.querySelectorAll('.menu');
-                const mSub = document.querySelectorAll('.submenu');
-                const currentLink = event.target;   
-                const SparentItem = currentLink.parentNode.parentNode;
-                const submenuitem = SparentItem.querySelector('.submenuitem');
-
-                    if (parentItem) {
-                        parentItem.focus();
-                        Esub[1].setAttribute('aria-expanded','false');
-                        Esub[2].setAttribute('aria-expanded','false');
-                    }
-                    
-                    if (SparentItem) {
-                        submenuitem.style.display = 'none';
-                    }
-        break;
+      if (isSubMenuOpen) {
+        submenu.classList.remove('open');
+        submenu.previousElementSibling.focus();
+        event.preventDefault();
+      }
+      break;
     }
-}
-
-// Function to handle submenu navigation
-function handleSubmenuNavigation(event) {
-    const key = event.key;
-    const currentLink = event.target;
-    const parentItem = currentLink.parentNode.parentNode;
-    const submenu = parentItem.querySelector('.submenu');
-    const submenuItems = submenu.querySelectorAll('a');
-    const submenuitem = parentItem.querySelector('.submenuitem');
-    
-    switch (event.key) {
-        case 'ArrowDown':
-            if (key === 'ArrowDown') {
-              const currentIndex = Array.from(submenuItems).indexOf(currentLink);
-              const nextIndex = (currentIndex + 1) % submenuItems.length;
-              const nextLink = submenuItems[nextIndex];
-              nextLink.focus();
-              submenuItems.forEach(item => item.setAttribute('tabindex', '-1'));
-              nextLink.setAttribute('tabindex', '0');
-              submenuitem.style.display = 'none';
-            }
-        break;
-
-        case 'ArrowUp':
-            if (key === 'ArrowUp') {
-                const nextLink = currentLink.nextElementSibling;
-                const prevLink = currentLink.previousElementSibling;
-                    if (prevLink) {
-                        prevLink.focus();
-                        prevLink.setAttribute('tabindex','0');
-                        currentLink.setAttribute('tab index','-1');
-                        nextLink.setAttribute('tabindex','-1');
-                    } else {
-                          document.querySelector('.submenu1').focus();
-                          document.querySelector('.submenu2').focus();
-                          document.querySelector('.submenuitem2').focus();
-                          event.stopPropagation();
-                    }
-            }
-        break;
-
-        case 'ArrowRight':
-            if (key === 'ArrowRight') {
-                event.preventDefault();
-                
-                const subsubmenuItems = submenuitem.querySelectorAll('a');
-                if (subsubmenuItems.length > 0) {
-                    submenuitem.style.display = 'flex';
-                    subsubmenuItems[0].focus();
-                    subsubmenuItems.forEach(item => item.setAttribute('tabindex', '-1'));
-                    subsubmenuItems[0].setAttribute('tabindex', '0');
-                }
-                
-            }
-        break;
-
-        case 'ArrowLeft':
-            if (key === 'ArrowLeft') {
-                const prevItem = parentItem.previousElementSibling;
-                const prevLink = prevItem.querySelector('a');
-                const Esub = document.querySelectorAll('.menu');
-                    if (prevItem) {
-                        prevLink.focus();
-                        Esub[1].setAttribute('aria-expanded', 'false');
-                        Esub[2].setAttribute('aria-expanded', 'false');
-                        submenu.style.display = 'none';
-                    }
-            }
-        break;
-    }
-}
-
-
-
-    function closeSubmenus(event) {
-      const submenu = document.querySelectorAll('.submenu');
-
-      submenu.forEach ((submenu) =>{
-        submenu.style.display='none';
-      });   
-    }
-
-    // Attach event listeners to submenu items
-    const submenuItems = document.querySelectorAll('.submenu a');
-        submenuItems.forEach((item) => {
-          item.addEventListener('keydown', handleSubmenuNavigation);
-        });
-    
-    // Attach event listeners to menu items
-    const menuItems = document.querySelectorAll('.menubar a');
-        menuItems.forEach((item) => {
-          item.addEventListener('keydown', handleNavigation);
-        });
-
-    const subitem = document.querySelectorAll('.submenuitem a');
-        subitem.forEach((item) => {
-          item.addEventListener('keydown', handleSubmenuNavigation);
-        });
+  }
